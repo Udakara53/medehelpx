9aae4b13bbe9c8dc6b7a16bea8a22a3fe9d4400d
Tharindu Udakara <udakaratharindu@gmail.com>
2023-03-24 23:35:14 +0530

    signUpForm is completed.

--- src/com/developersstack/controller/LoginFormController.java
+++ src/com/developersstack/controller/LoginFormController.java
@@ -7,14 +7,22 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public JFXTextField txtEmail;
     public JFXPasswordField txtPassword;
     public JFXRadioButton rBtnDoctor;
     public ToggleGroup accountType;
+    public AnchorPane loginContext;
 
     public void signInOnAction(ActionEvent actionEvent) {
         String email =txtEmail.getText().toLowerCase().trim();
@@ -30,8 +38,9 @@ public class LoginFormController {
                 if (dto.getPassword().equals(password)){
                     if (dto.getAccountType().equals(accountType)) {
                         new Alert(Alert.AlertType.CONFIRMATION,"LoginSuccess!").show();
+                        return;
                     }else{
-                        new Alert(Alert.AlertType.WARNING,String.format("We can't find Your % Account ",accountType)).show();
+                        new Alert(Alert.AlertType.WARNING,String.format("We can't find Your %s Account ",accountType)).show();
                         return;
                     }
                 }else{
@@ -39,11 +48,14 @@ public class LoginFormController {
                     return;
                 }
             }
-            new Alert(Alert.AlertType.WARNING,String.format("we can't find % email ",email)).show();
-            return;
-        }
+        }new Alert(Alert.AlertType.WARNING,String.format("we can't find %s address ",email)).show();
+
     }
 
-    public void createAnAccountOnAction(ActionEvent actionEvent) {
+    public void createAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) loginContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/SignUpForm.fxml"))));
+        stage.centerOnScreen();
+        stage.show();
     }
 }
---
+++ src/com/developersstack/controller/SignUpFormController.java
@@ -0,0 +1,56 @@
+package com.developersstack.controller;
+
+import com.developersstack.db.Database;
+import com.developersstack.entity.User;
+import com.developersstack.enums.AccountType;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.util.Optional;
+
+public class SignUpFormController {
+    public AnchorPane signupContext;
+    public JFXTextField txtFirstName;
+    public JFXTextField txtLastName;
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtPassword;
+    public JFXRadioButton rBtnDoctor;
+
+    public void signupOnAction(ActionEvent actionEvent) throws IOException {
+        String firstName = txtFirstName.getText().trim();
+        String lastName = txtLastName.getText().trim();
+        String email = txtEmail.getText().trim().toLowerCase();
+        String password = txtPassword.getText().trim();
+        AccountType accountType = rBtnDoctor.isSelected()?AccountType.DOCTOR:AccountType.PATIENT;
+        if (isExistsEmail(email)){
+            new Alert(Alert.AlertType.WARNING,"email address already exists!").show();
+            return;
+        }
+        Database.userTable.add(new User(firstName,lastName,email,password,accountType));
+        new Alert(Alert.AlertType.CONFIRMATION,"Sign up success!").show();
+        setUi();
+    }
+    private boolean isExistsEmail(String email){
+        Optional<User> findUser = Database.userTable.stream().filter(e -> e.getEmail().equals(email)).findFirst();
+        return findUser.isPresent();
+    }
+
+    public void alreadyHaveAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi();
+    }
+    private void setUi() throws IOException {
+        Stage stage= (Stage) signupContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"))));
+        stage.centerOnScreen();
+        stage.show();
+    }
+}
