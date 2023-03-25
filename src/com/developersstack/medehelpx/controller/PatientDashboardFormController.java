d30d834231d4f475a21261fdd45a269cb1f394cf
Tharindu Udakara <udakaratharindu@gmail.com>
2023-03-25 23:36:47 +0530

    Ready to convert JDBC

---
+++ src/com/developersstack/medehelpx/controller/PatientDashboardFormController.java
@@ -0,0 +1,18 @@
+package com.developersstack.medehelpx.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class PatientDashboardFormController {
+
+    public AnchorPane patientDashboardContext;
+    public Label lblDate;
+    public Label lblTime;
+
+    public void logOutOnAction(ActionEvent actionEvent) {
+    }
+
+    public void navigationToNewAppoinmentPage(ActionEvent actionEvent) {
+    }
+}
