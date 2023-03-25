211d566764e903ceec28ed6e543bc3bebc19b22a
Tharindu Udakara <udakaratharindu@gmail.com>
2023-03-26 01:34:30 +0530

    Ensure password encryption, PasswordConfig.java

--- src/com/developersstack/medehelpx/util/PasswordConfig.java
+++ src/com/developersstack/medehelpx/util/PasswordConfig.java
@@ -1,12 +1,11 @@
 package com.developersstack.medehelpx.util;
-
 import org.mindrot.BCrypt;
 
 public class PasswordConfig {
     public String encrypt(String text){
         return BCrypt.hashpw(text,BCrypt.gensalt(10));
     }
-    public boolean encrypt(String rowPassword,String encryptedPassword){
+    public boolean decrypt(String rowPassword,String encryptedPassword){
         return BCrypt.checkpw(rowPassword,encryptedPassword);
     }
 }
